2efc03ae5495c48ed04cf3aab5b70a81d142a9c0
nnicha123 <nicha_ngamtweerat@hotmail.com>
2024-09-30 14:55:00 +0700

    Added ngrx devtools, store and effects to app module

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,9 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { APP_ROUTES } from './app.routes';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+
+const NGRX_MODULES = [
+  StoreModule.forRoot({}, {}),
+  EffectsModule.forRoot([]),
+  StoreDevtoolsModule.instrument({ maxAge: 25, name: 'Maths Book Project Dev Tool', logOnly: !isDevMode() })
+]
 
 @NgModule({
   declarations: [
@@ -11,7 +20,8 @@ import { APP_ROUTES } from './app.routes';
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(APP_ROUTES)
+    RouterModule.forRoot(APP_ROUTES),
+    ...NGRX_MODULES
   ],
   providers: [],
   bootstrap: [AppComponent]
