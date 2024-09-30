1ac0019c06fe34c1410c60ffba84c32a90565df4
nnicha123 <nicha_ngamtweerat@hotmail.com>
2024-09-30 14:28:56 +0700

    Added simple backend endpoints that reads from json files

---
+++ Backend/app.js
@@ -0,0 +1,83 @@
+import express from 'express';
+import cors from 'cors';
+import fs from 'fs';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("Checking it works!");
+})
+    .post("/auth/login", loginUser)
+    .get('/user/users', getUsers)
+    .get('/user/userSingle/:userId', getSingleUser)
+    .get('/user/userExercise/:userId', getUserExercises)
+    .get('/exercise/:exerciseId', getQuestionsFromExerciseId)
+
+app.listen(3000, () => {
+    console.log('Listening at: http://localhost:3000');
+})
+
+function getUsers(req, res) {
+    fs.readFile("users.json", { encoding: "utf-8" }, (err, results) => {
+        // console.log(results);
+        let userList = JSON.parse(results);
+        res.send(userList);
+    })
+}
+
+function getSingleUser(req, res) {
+    fs.readFile("users.json", { encoding: "utf-8" }, (err, results) => {
+        let userId = 1;
+        if (req.params.userId) {
+            userId = +req.params.userId;
+        }
+        let userList = JSON.parse(results);
+        let singleUser = userList.filter(row => row.userId === userId)[0];
+        res.send(singleUser);
+    })
+}
+
+function loginUser(req, res) {
+    // To add security later
+    const { username, password } = req.body;
+    fs.readFile("users.json", { encoding: "utf-8" }, (err, results) => {
+        let userList = JSON.parse(results);
+        let singleUser = userList.filter(row => row.username === username && row.password === password)[0];
+        if (singleUser) {
+            res.send(singleUser);
+        } else {
+            res.status(401).send({ "message": "Unauthorized: Cannot find username password combination" })
+        }
+    })
+}
+
+function getUserExercises(req, res) {
+    fs.readFile("exercise.json", { encoding: "utf-8" }, (err, results) => {
+        let exerciseList = JSON.parse(results);
+        if (req.params.userId) {
+            let userId = +req.params.userId;
+            const userExercises = exerciseList.filter(row => row.userId === userId);
+            res.send(userExercises);
+        } else {
+            res.status(500).send({ "message": "Cannot find exercises related to userId" })
+        }
+
+    })
+}
+
+function getQuestionsFromExerciseId(req,res) {
+    fs.readFile("question.json", { encoding: "utf-8" }, (err, results) => {
+        let questionsList = JSON.parse(results);
+        if (req.params.exerciseId) {
+            let exerciseId = +req.params.exerciseId;
+            const questions = questionsList.filter(row => row.exerciseId === exerciseId);
+            res.send(questions);
+        } else {
+            res.status(500).send({ "message": "Cannot find questions related to exerciseId" })
+        }
+
+    })
+}
